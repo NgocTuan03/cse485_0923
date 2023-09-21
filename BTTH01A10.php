64e6b0dc70c600b1df006af44dd1d68c3e431abc
NgocTuan03 <145407752+NgocTuan03@users.noreply.github.com>
2023-09-22 02:07:30 +0700

    Add files via upload

---
+++ BTTH01A10.php
@@ -0,0 +1,5 @@
+<?php
+    $arrs = "[a, b, ABC]";
+    $arrs_upper = strtoupper($arrs);
+    echo $arrs_upper; // In ra: "[A, B, ABC]"
+?>
---
+++ BTTH01A9.php
@@ -0,0 +1,5 @@
+<?php
+    $arrs = "[a, b, ABC]";
+    $arrs_lower = strtolower($arrs);
+    echo $arrs_lower; // In ra: "[a, b, abc]"
+?>
